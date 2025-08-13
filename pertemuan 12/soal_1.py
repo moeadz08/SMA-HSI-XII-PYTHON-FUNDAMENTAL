697d3a9341151418ad8466bf4113dd0e92a6ea6b
moeadz08 <adzmoe08@gmail.com>
2025-08-13 09:37:15 +0700

    first commit

--- pertemuan 12/soal_1.py
+++ pertemuan 12/soal_1.py
@@ -1,14 +1,3 @@
-# nama_file = "log_kegiatan.txt"
-
-# user = input('Masukkan kegiatan: ')
-# with open(user, 'a') as file:
-#  print(f"\nMenambahkan item baru ke '{nama_file}'...")
-
-#  file.write(user)
-# #  file.write("5. Telur (1 lusin)\n")
-#  print("Berhasil menambahkan item baru.")
-# print(f"File '{nama_file}' telah ditutup.")
-
 # change directory
 "cd '.\pertemuan 12\'"
 
@@ -24,6 +13,3 @@ with open(nama_file, 'a') as file:  # pakai nama_file, bukan user
         print("Berhasil menambahkan item baru.")
 
 print(f"File '{nama_file}' telah diubka.")
-
-
-
